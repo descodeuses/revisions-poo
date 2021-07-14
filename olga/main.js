c21df3794bc1a7e2420ffc96a2e45a7f74dd9d56
olga <>
2021-07-14 16:52:07 +0200

    creation de la class Vehicle et sous-class Bus, ajout des commentaires

--- olga/main.js
+++ olga/main.js
@@ -1,3 +1,4 @@
+// Création de la class Vehicles avec trois paramètres
 function Vehicles(name, kilometer, maxSpeed){
   this.name = name;
   this.kilometer = kilometer;
@@ -6,7 +7,7 @@ function Vehicles(name, kilometer, maxSpeed){
 const moto = new Vehicles('Toto', 300, 150);
 
 
-
+// Création de la class avec le constructeur 
 class Vehicle {
   constructor(name, kilometer, maxSpeed) {
     this.name = name;
@@ -14,12 +15,14 @@ class Vehicle {
     this.maxSpeed = maxSpeed;
   }
 }
-
+// La nouvelle class Bus qui hérites des paramètres de la class Vehicle
 class Bus extends Vehicle {
   constructor(name, kilometer, maxSpeed) {
     super(name, kilometer, maxSpeed); // appelle le constructeur parent avec le paramètre
   }
+// Une nouvelle fonction qui est propre au Bus
   sites(siege) {
+  // la valeur par defaut est 75 si ce n'est pas préciser dans les paramètres
     if(siege == null){
       siege = 75;
     }
